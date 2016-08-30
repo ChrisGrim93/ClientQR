92b26e823111c0c1f5bfbdb87d34b0e51aeeaf21
Chris Grimward <chrisg@dev.com>
2016-08-30 11:36:40 +0100

    Additional changes to push to heroku

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('qr')
+  res.render('qr_test')
 })
 
 // Example routes - feel free to delete these
